c193e362f43e177c829f1527a63255c4add79687
Kavya Patel <webdevloper07070@gmail.com>
2026-04-05 10:40:54 +0530

    Add route to regenerate pair code
    Added a new route to regenerate pair code.

--- routes/pairRoutes.js
+++ routes/pairRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { pairUsers, getPairedUsers, unpairUser } = require('../controllers/pairController');
+const { pairUsers, getPairedUsers, unpairUser, regenerateCode } = require('../controllers/pairController');
 const { protect } = require('../middleware/auth');
 
 // POST /api/pair - Pair with another user using code
 router.post('/', protect, pairUsers);
+
+// POST /api/pair/regenerate-code - Regenerate pair code
+router.post('/regenerate-code', protect, regenerateCode);
 
 // GET /api/pair/users - Get list of paired users
 router.get('/users', protect, getPairedUsers);
